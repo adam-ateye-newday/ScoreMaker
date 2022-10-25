af2a85028374a4089ca5bf06fe193650260578f0
Adam Ateye <S01760@newday.co.uk>
2022-10-25 10:40:51 +0100

    HeartbeatController added

---
+++ ScoremakerAPI/Controllers/HeartbeatController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScoreMaker.API.Controllers
+{
+    /// <summary>
+    /// Class HeartbeatController.
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.Controller" />
+    [Route("api/[controller]")]
+    public class HeartbeatController : Controller
+    {
+        /// <summary>
+        /// Gets this instance.
+        /// </summary>
+        /// <returns>System.String.</returns>
+        [HttpGet]
+        public string Get() => "OK";
+    }
+}
